23a1695a3f347d3f44b6085af162c40eaddd56c9
liton61 <litonofficial61@gmail.com>
2023-11-24 03:07:27 +0600

    error page added

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+import error from '../../assets/4O4.jpg';
 
 
 const Error = () => {
     return (
-        <div>
-            <h1>Error</h1>
+        <div className="flex justify-center items-center h-screen">
+            <div>
+                <img src={error} alt="" />
+                <div className="flex justify-center items-center mt-5">
+                    <Link to="/">
+                        <button className="text-lg font-semibold">Back to Home</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
