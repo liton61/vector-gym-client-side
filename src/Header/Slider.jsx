a2f3198419fa8c15869f1a2841dacdfbee93d5d2
liton61 <litonofficial61@gmail.com>
2023-12-13 13:50:46 +0600

    type writer added

--- src/Header/Slider.jsx
+++ src/Header/Slider.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Typewriter } from "react-simple-typewriter";
 
 
 const Slider = () => {
@@ -7,10 +8,19 @@ const Slider = () => {
             <div className="hero-overlay bg-opacity-50"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Building a Healthier Lifestyle</h1>
+                    <h1 className="mb-5 text-5xl font-bold">
+                        <Typewriter
+                            words={["Building a Healthier Lifestyle"]}
+                            loop={5}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={70}
+                            deleteSpeed={50}
+                            delaySpeed={1000}
+                        /></h1>
                     <p className="mb-5">A gym is a dedicated space designed to support physical fitness and health goals. Equipped with a variety of exercise machines, weights, and facilities, it provides an environment where individuals engage in workouts, strength training, cardio exercises, and group fitness classes.</p>
                     <Link to="/classes">
-                    <button className="btn btn-active btn-secondary">Learn More <i className="fa-solid fa-arrow-right"></i></button>
+                        <button className="btn btn-active btn-secondary">Learn More <i className="fa-solid fa-arrow-right"></i></button>
                     </Link>
                 </div>
             </div>
