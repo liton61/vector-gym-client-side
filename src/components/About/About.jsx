2bbb1fb969626750e604ba68b785aa9a20f021dd
liton61 <litonofficial61@gmail.com>
2024-01-27 20:02:17 +0600

    responsive design added for about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,24 +6,23 @@ import profile3 from '../../assets/profile3.png';
 const About = () => {
     return (
         <section className="bg-gray-100 py-12">
-            <div className="container mx-auto px-4">
-                <div className="flex flex-col md:flex-row items-center justify-between">
-                    <div className="md:w-1/2">
-                        <img src={about} alt="Gym" className="w-full rounded-lg shadow-md mb-6 md:mb-0" />
+            <div className="lg:w-3/4 mx-auto">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:px-0 px-5">
+                    <div>
+                        <img src={about} alt="Gym" className="w-full rounded" />
                     </div>
-                    <div className="md:w-1/2 md:pl-10">
-                        <h1 className="text-4xl text-center font-semibold border-b-4 border-yellow-500 w-60 mx-auto pb-3 m-10">About Us</h1>
-                        <p className="text-gray-700 mb-6 text-justify">
-                            Founded in 2009, Vector Gym is dedicated to providing a holistic fitness experience for individuals of all levels.
-                            We are committed to helping you achieve your fitness goals and fostering a healthier lifestyle.
-                        </p>
-                        <p className="text-gray-700 mb-6 text-justify">
-                            Our facilities boast state-of-the-art equipment, diverse fitness classes, expert trainers, and a welcoming community atmosphere.
-                            At Vector gym, we believe in making fitness enjoyable, effective, and accessible to everyone.
-                        </p>
-                        <p className="text-gray-700 mb-6 text-justify">
-                            Safety and hygiene are our top priorities. We adhere to stringent cleanliness protocols and prioritize our members well-being.
-                        </p>
+                    <div>
+                        <div>
+                        <h1 className="text-3xl text-center font-semibold mb-3">About Us</h1>
+                            <p className="text-gray-700 mb-2 text-justify">
+                                Founded in 2009, Vector Gym is dedicated to providing a holistic fitness experience for individuals of all levels.
+                                We are committed to helping you achieve your fitness goals and fostering a healthier lifestyle.
+                            </p>
+                            <p className="text-gray-700 mb-2 text-justify">
+                                Our facilities boast state-of-the-art equipment, diverse fitness classes, expert trainers, and a welcoming community atmosphere.
+                                At Vector gym, we believe in making fitness enjoyable, effective, and accessible to everyone.
+                            </p>
+                        </div>
                         <div>
                             <div className="avatar-group -space-x-6 rtl:space-x-reverse">
                                 <div className="avatar">
